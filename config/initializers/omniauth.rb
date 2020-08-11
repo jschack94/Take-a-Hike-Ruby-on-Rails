4ed541b5652a3017bc9ec1979c3ebce02d952963
Jonathan <jschack94@gmail.com>
2020-08-11 14:53:43 -0500

    new

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, '3371647822898573
-    ', 'b572b8d2dc96bea05c7c2536239cfdd9'
+    provider :facebook, '3371647822898573', 'b572b8d2dc96bea05c7c2536239cfdd9'
   end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_14_175735) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
